dfe2f61a542a868a715788f6bd65994eea5105d8
efredz <efredzb@gmail.com>
2019-05-26 21:19:23 -0400

    Url parsing

--- property_scrapper/spiders/portal_inmobiliario_spider.py
+++ property_scrapper/spiders/portal_inmobiliario_spider.py
@@ -11,8 +11,10 @@ class PortalInmobiliario(scrapy.Spider):
     def parse(self, response):
         full_results = response.xpath('//div[@class="col-sm-9 product-item-data"]')
         for pi_property in full_results:
-            print(get_location(pi_property).extract())
-            print(get_price(pi_property).extract())
+            print(get_location(pi_property).extract_first())
+            print(get_price(pi_property).extract_first())
+            print(get_square_meters(pi_property).extract_first())
+            print(get_link(pi_property, response))
 
 
 def get_location(selector):
@@ -20,11 +22,14 @@ def get_location(selector):
 
 
 def get_price(selector):
-    return selector.xpath('.//p[@class="product-property"]/span/text()')
+    return selector.xpath('.//span[@class="product-property-value product-price-value"]/text()')
 
 
-class Prop:
-    def __init__(self, name="", tipo="", indice=""):
-        self.name = name
-        self.tipo = tipo
-        self.indice = indice
+def get_square_meters(selector):
+    return selector.xpath('.//span[@class="product-property-value"]/text()')
+
+
+def get_link(selector, response):
+    relative_url = selector.xpath('.//h4/a/@href').extract_first()
+    return response.urljoin(str(relative_url))
+
